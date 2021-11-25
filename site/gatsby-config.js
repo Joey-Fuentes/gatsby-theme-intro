4bde91a83e9c80f11b4fb5d044c6d526841184b0
Steven Roh <hello@stevenroh.ch>
2021-11-25 15:17:50 +0100

    Update version

--- site/gatsby-config.js
+++ site/gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: "@wkocjan/gatsby-theme-intro",
+      resolve: "@rohs/gatsby-theme-intro",
       options: {
         basePath: "/",
         contentPath: "content/",
