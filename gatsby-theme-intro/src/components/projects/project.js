971e387ec9f534e22dba521320fad2d014f558fa
Joe Fuentes <u.j.fuentes@gmail.com>
2023-12-11 00:43:35 -0500

    temp commit to test slick slides

--- gatsby-theme-intro/src/components/projects/project.js
+++ gatsby-theme-intro/src/components/projects/project.js
@@ -1,9 +1,16 @@
 import { GatsbyImage } from "gatsby-plugin-image"
 import React from "react"
+import Slider from "react-slick"
 import { ProjectType } from "../../types"
 import ProjectIcon from "./project-icon"
 import ProjectStatus from "./project-status"
 import ProjectTags from "./project-tags"
+
+const settings = {
+  dots: false,
+  infinite: true,
+  autoplay: true,
+}
 
 const Project = props => {
   const { name, image, url, description, status, tags, icon } = props
@@ -11,7 +18,10 @@ const Project = props => {
     <div className="border-t-4 border-line relative flex flex-wrap bg-back-light p-4 lg:p-8 bg-no-repeat text-sm mb-6">
       {image && (
         <div className="w-full pb-4 lg:w-2/5 lg:pr-8 lg:pb-0">
-          <GatsbyImage image={image.childImageSharp.gatsbyImageData} alt={name} />
+          <Slider {...settings} className="overflow-hidden">
+            <GatsbyImage image={image.childImageSharp.gatsbyImageData} alt={name} />
+            <GatsbyImage image={image.childImageSharp.gatsbyImageData} alt={name} />
+          </Slider>
         </div>
       )}
       <div className="lg:flex-1">
